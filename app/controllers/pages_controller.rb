e61d20c812c88e6247ddb46b1dd4c1b0c492bff7
haseebc <haseebchaudhary@gmail.com>
2021-05-26 18:32:10 +0200

    update Wednesday

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,22 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: %i[home our_work success_stories services blog contact]
 
   def home
   end
+
+  def our_work
+  end
+
+  def success_stories
+  end
+
+  def services
+  end
+
+  def blog
+  end
+
+  def contact
+  end
+
 end
